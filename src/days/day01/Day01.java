81e5557f36ed444fc7357286022d7c2b15615af7
Holly <hollyxue05@gmail.com>
2025-12-01 21:38:30 -0500

    day one part one

--- src/days/day01/Day01.java
+++ src/days/day01/Day01.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Day01 {
 
     public static ArrayList<String> loadInput(String filePath) {
-        filePath = "../../inputs/day01Input.txt"; // + filePath;
+        filePath = "../../inputs/" + filePath;
         //System.out.println("filepath: " + filePath);
         ArrayList<String> returnStringArray = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
@@ -20,5 +20,53 @@ public class Day01 {
         }
 
         return returnStringArray;
+    }
+
+    public static int password(ArrayList<String> list) {
+        int curr = 50;
+        int zeroCount = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            int rotation = Integer.parseInt(list.get(i).substring(1));
+            int direction; 
+            if (list.get(i).substring(0, 1).contains("R"))
+                direction = 0;
+            else
+                direction = 1; 
+
+            int[] output = dialer(direction, rotation, curr, false);
+
+            curr = output[0];
+            zeroCount += output[1];
+        }
+
+        return zeroCount;
+    }
+
+    private static int[] dialer(int direction, int rotations, int dial, boolean modified) {
+        // right = 0, left = 1
+        // dial = [0], zeroCount = [1]
+
+        int newDial = dial;
+        int zeroCount = 0;
+        for (int i = 0; i < rotations; i++) {
+            if (direction == 0) 
+                newDial++;
+            else 
+                newDial--;
+
+            if (newDial == 100) 
+                newDial = 0;
+            else if (newDial == -1) 
+                newDial = 99;
+
+            if (modified && newDial == 0) 
+                zeroCount++;
+        }
+
+        if (newDial == 0 && !modified) 
+            zeroCount++;
+
+        return new int[]{newDial, zeroCount};
     }
 }
--- src/days/day01/Main.java
+++ src/days/day01/Main.java
@@ -3,7 +3,10 @@ import java.util.ArrayList;
 class Main {
     public static void main(String[] args) {
         ArrayList<String> input = Day01.loadInput("day01Input.txt");
-        System.out.println(input.toString());
+        
+        // part one 
+        int zeroCount = Day01.password(input); 
+        System.out.println("password: "+zeroCount);
 
     }
 }
